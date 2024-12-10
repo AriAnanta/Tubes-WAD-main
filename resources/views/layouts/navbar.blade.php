06bd55017781b9bc18ef27b2f42bdf6064f1d68c
Ari Ananta <madeariananta@gmail.com>
2024-12-10 10:32:50 +0700

    Update

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,21 +2,27 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
     <div class="container-fluid">
+        <!-- Tombol Toggle untuk Layar Kecil -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <span class="navbar-brand mb-0 h1">Dashboard</span>
+        
+        <!-- Brand Nama Halaman -->
+        <a class="navbar-brand mb-0 h1" href="#" style="font-family: 'Zen Dots', cursive;">Dashboard</a>
+
+        <!-- Menu Navbar -->
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    
-                </li>
+            <ul class="navbar-nav ms-auto align-items-center">
+                
+
+                <!-- Dropdown Profil -->
                 <li class="nav-item">
                     <div class="dropdown">
                         <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="bi bi-person-circle"></i>
                         </button>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
                             <li><a class="dropdown-item" href="#">Logout</a></li>
                         </ul>
                     </div>
